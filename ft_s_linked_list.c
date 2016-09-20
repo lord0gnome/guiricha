3df1c334ebbf38a2ffb2fde1f6f5f85206157987
Guillaume RICHARD <guiricha@e3r9p12.42.fr>
2016-09-20 11:46:27 +0200

    scripted push message, no really though this is a script

--- ft_s_linked_list.c
+++ ft_s_linked_list.c
@@ -6,7 +6,7 @@
 /*   By: guiricha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/20 13:01:49 by guiricha          #+#    #+#             */
-/*   Updated: 2016/09/19 13:52:31 by guiricha         ###   ########.fr       */
+/*   Updated: 2016/09/20 11:22:18 by guiricha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void		ft_print_members(t_s_list *list)
 {
 	while (list)
 	{
+		ft_putnbr(list->flag);
+		ft_putchar('_');
 		ft_putendl(list->str);
 		list = list->next;
 	}
