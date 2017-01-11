5a106dbf4938ce837635b93962bee4894da54705
Guillaume RICHARD <guiricha@e2r11p1.42.fr>
2017-01-11 16:15:57 +0100

    ILikecommiting

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: guiricha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 15:39:30 by guiricha          #+#    #+#             */
-/*   Updated: 2016/12/03 13:32:00 by guiricha         ###   ########.fr       */
+/*   Updated: 2017/01/11 16:13:13 by guiricha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,6 +96,7 @@ void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+void				ft_lst_pushback(t_list **alst, void *c, size_t c_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(f)(t_list *elem));
 int					ft_isspace(char c);
 char				**ft_split(char *s);
